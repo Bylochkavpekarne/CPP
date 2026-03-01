748a0be3ce53a6bafc553d1bac871f8dc263c6d1
bylochka <kirill_kirillovich@ukr.net>
2026-03-01 15:59:18 +0200

    Firs commit

---
+++ HM1/HM1/Source.cpp
@@ -0,0 +1,22 @@
+#include "iostream"
+#include "Windows.h"
+
+int main()
+{
+	using namespace std;
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+	cout << " Кінець фільму, погас екран\n Лишився порожнім кінозал\n Всі наші з тобою спільні плани\n Виявились самообманом\n Кінець фільму, погас екран\n Лишився порожнім кінозал\n Всі наші з тобою спільні плани\n Виявились самообманом " << endl;
+	
+	cout << "\nEvery\n	hunter\n		wants\n	to\nknow\n	where\n		the\n	pheasant\nis." << endl;
+
+	cout << "\n Продам квартиру" << endl;
+	cout << " У центрі міста\n З ремонтом\n 5хв до метро" << endl;
+	cout << " Ціна: 2.500.000грн" << endl;
+	cout << " Тел.: +380777777777" << endl;
+	cout << " 777 | 777 | 777\n 777 | 777 | 777\n 777 | 777 | 777\n 777 | 777 | 777\n 777 | 777 | 777" << endl;
+
+
+
+	return 0;
+}
